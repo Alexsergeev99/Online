1e3e384b5e2b0497f8c81b3fc8f7efa788149a34
Alex Sergeev <sanhokk@yandex.ru>
2023-10-13 18:24:10 +0300

    New commit

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,8 +1,8 @@
 fun main() {
-println(agoToText(20000))
+println(agoToText(840))
 }
 fun agoToText(seconds: Int): String {
-    var result = when (seconds) {
+    val result = when (seconds) {
         in 0..60 -> "был(а) в сети только что"
         in 60..60 * 60 -> "был(а) в сети ${seconds/60} ${correctFormMinute(seconds)} назад"
         in 60 * 60..24 * 60 * 60 -> "был(а) в сети ${seconds/60/60} ${correctFormHour(seconds)} назад"
@@ -15,17 +15,17 @@ fun agoToText(seconds: Int): String {
 }
 
 fun correctFormMinute(seconds: Int): String {
-    var correct = when {
-        (seconds / 60)%10 == 1 && (seconds / 60) % 100 != 11 ->  "минуту"
-        (seconds / 60) % 10 == 2 || (seconds / 60) % 10 == 3 || (seconds / 60) % 10 == 4 -> "минуты"
+    val correct = when {
+        (seconds / 60) % 10 == 1 && (seconds / 60) % 100 != 11 ->  "минуту"
+        (seconds / 60) % 10 in 2..4 && (seconds / 60) % 100 !in 12..14  -> "минуты"
         else -> "минут"
     }
     return correct
 }
 fun correctFormHour(seconds: Int): String {
-    var correctHour = when {
+    val correctHour = when {
         (seconds / 60 / 60)%10 == 1 && (seconds / 60 / 60) % 100 != 11 ->  "час"
-        (seconds / 60 / 60) % 10 == 2 || (seconds / 60 / 60) % 10 == 3 || (seconds / 60 / 60) % 10 == 4 -> "часа"
+        (seconds / 60 / 60) % 10 in 2..4  && (seconds / 60 / 60) % 100 !in 12..14  -> "часа"
         else -> "часов"
     }
     return correctHour
